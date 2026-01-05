9739a973c6d3cbba83219b5b83c83f9a17d08c8b
meehakimov <meehakimov@gmail.com>
2026-01-05 23:28:51 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,74 @@
+window.addEventListener("DOMContentLoaded", function () {
+  window.addEventListener("scroll", function () {
+    const header = document.querySelector("header");
+    header.classList.toggle("sticky", window.scrollY > 0);
+  });
+
+  const menuBtn = document.querySelector(".menu-btn");
+  const navigation = document.querySelector(".navigation");
+  const navigationItems = document.querySelectorAll(".navigation a");
+
+  menuBtn.addEventListener("click", () => {
+    menuBtn.classList.toggle("active");
+    navigation.classList.toggle("active");
+  });
+
+  navigationItems.forEach((navItem) => {
+    navItem.addEventListener("click", () => {
+      menuBtn.classList.remove("active");
+      navigation.classList.remove("active");
+    });
+  });
+
+  const scrollBtn = document.querySelector(".scrollToTop-btn");
+  window.addEventListener("scroll", () => {
+    scrollBtn.classList.toggle("active", window.scrollY > 500);
+  });
+  scrollBtn.addEventListener("click", () => {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+  });
+
+  window.addEventListener("scroll", () => {
+    let reveals = document.querySelectorAll(".reveal");
+
+    for (let i = 0; i < reveals.length; i++) {
+      let windowHeight = window.innerHeight;
+      let revealTop = reveals[i].getBoundingClientRect().top;
+      let revealPoint = 50;
+
+      if (revealTop < windowHeight - revealPoint) {
+        reveals[i].classList.add("active");
+      }
+    }
+  });
+});
+const TOKEN = "8324337276:AAFDbj4hGvCyMufcRtEdPCPgZgRCi88jGLc";
+const CHAT_ID = "5777607887";
+
+document.getElementById("row").addEventListener("submit", function(e){
+  e.preventDefault();
+
+  let name = document.getElementById("name").value;
+  let email = document.getElementById("email").value;
+  let message = document.getElementById("message").value;
+
+  let text = `
+🧑 Ism: ${name}
+📧 Email: ${email}
+💬 Xabar: ${message}
+  `;
+
+  fetch(`https://api.telegram.org/bot${TOKEN}/sendMessage`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify({
+      chat_id: CHAT_ID,
+      text: text
+    })
+  })
+  .then(res => alert("Xabar yuborildi ✅"))
+  .catch(err => alert("Xatolik ❌"));
+});
